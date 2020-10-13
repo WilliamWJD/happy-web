1917791617b32f89c5db6c3be689cb84acf06ed9
williamwjd <william.jose.wjd@gmail.com>
2020-10-13 08:27:15 -0300

    configurando eslint, prettier, editorConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const App:React.FC = () =>{
-  return <h1>Happy</h1>
-}
+const App: React.FC = () => {
+  return <h1>Happy</h1>;
+};
 
 export default App;
